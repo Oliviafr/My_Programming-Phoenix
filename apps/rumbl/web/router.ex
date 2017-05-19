6c4ffc81cbc16a739fae55508b4d16de1a30f3ea
Oliviafr <ofrumin@gmail.com>
2017-05-19 16:24:23 -0700

    First commit

---
+++ apps/rumbl/web/router.ex
@@ -0,0 +1,78 @@
+defmodule Rumbl.Router do
+  use Rumbl.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug Rumbl.Auth, repo: Rumbl.Repo
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", Rumbl do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
+    get "/watch/:id", WatchController, :show
+  end
+
+  scope "/manage", Rumbl do
+    pipe_through [:browser, :authenticate_user]
+
+    resources "/videos", VideoController
+  end
+end
+
+
+
+# defmodule Rumbl.Router do
+#   use Rumbl.Web, :router
+
+#   pipeline :browser do
+#     plug :accepts, ["html"]
+#     plug :fetch_session
+#     plug :fetch_flash
+#     plug :protect_from_forgery
+#     plug :put_secure_browser_headers
+#     plug Rumbl.Auth, repo: Rumbl.Repo
+#   end
+
+#   pipeline :api do
+#     plug :accepts, ["json"]
+#   end
+# scope "/", Rumbl do
+#     pipe_through :browser # Use the default browser stack
+
+#     get "/", PageController, :index
+#     resources "/users", UserController, only: [:index, :show, :new, :create]
+#     resources "/sessions", SessionController, only: [:new, :create, :delete]
+#   end
+
+#   scope "/manage", Rumbl do
+#     pipe_through [:browser, :authenticate_user]
+
+#     resources "/videos", VideoController
+#   end
+# end
+  
+#   scope "/manage", Rumbl do
+#     piple_through[:browser, :authenticate_user]
+#     resources "/videos", VideoController
+#   end
+
+#   scope "/", Rumbl do
+#     pipe_through :browser
+
+#     get "/", PageController, :index
+#     resources "/users", UserController, only: [:index, :show, :new, :create]
+#     resources "/sessions", SessionController, only: [:new, :create, :delete]
+#     resources "/videos", VideoController
+#   end
+# end
